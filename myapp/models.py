b9b7f68f99c2921373149022394ffce52761e349
Deepak Singh <111898996+dpk17github@users.noreply.github.com>
2025-11-11 10:36:21 +0530

    Add files via upload

---
+++ myapp/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+from django.db import models
+
+class UserAccount(models.Model):
+    username = models.CharField(max_length=150, unique=True)
+    password = models.CharField(max_length=200)
+
+    def __str__(self):
+        return self.username
