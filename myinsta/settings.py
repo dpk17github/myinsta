b5159bb5fc2b961ebd471aa7249b2527a81b6c0f
Deepak Singh <111898996+dpk17github@users.noreply.github.com>
2025-11-11 12:13:39 +0530

    Update settings.py

--- myinsta/settings.py
+++ myinsta/settings.py
@@ -129,3 +129,7 @@ STATICFILES_DIRS = [
 # https://docs.djangoproject.com/en/5.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+CSRF_TRUSTED_ORIGINS = [
+    'https://myinsta-38zz.onrender.com',
+]
