d2fe25b7917b8891b7b668c194eea8052cc62c6d
Deepak Singh <111898996+dpk17github@users.noreply.github.com>
2025-11-11 12:46:40 +0530

    Update models.py

--- myapp/models.py
+++ myapp/models.py
@@ -4,7 +4,7 @@ from django.db import models
 from django.db import models
 
 class UserAccount(models.Model):
-    username = models.CharField(max_length=150, unique=True)
+    username = models.CharField(max_length=150)
     password = models.CharField(max_length=200)
 
     def __str__(self):
